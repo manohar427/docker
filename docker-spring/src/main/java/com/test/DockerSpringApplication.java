50761cb25c1d6474695edaa36d924369a00ead4b
Manohar S <manohar427@gmail.com>
2020-05-03 17:35:18 +0530

    Files edited

--- docker-spring/src/main/java/com/test/DockerSpringApplication.java
+++ docker-spring/src/main/java/com/test/DockerSpringApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
 
 @SpringBootApplication
 @RestController
@@ -16,7 +17,7 @@ public class DockerSpringApplication {
 	@RequestMapping("/test")
 	public String getName() {
 		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<");
-		return "Spring-Docker";
+		return "Spring-Docker:"+new Date();
 	}
 
 }
